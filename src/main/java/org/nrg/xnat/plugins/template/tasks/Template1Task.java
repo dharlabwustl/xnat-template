dda6d7db6ae168827219f6e2bf6d41a4713dde18
dharlabwustl <atulkumar@wustl.edu>
2023-10-29 23:59:41 -0500

    First commit

--- src/main/java/org/nrg/xnat/plugins/template/tasks/Template1Task.java
+++ src/main/java/org/nrg/xnat/plugins/template/tasks/Template1Task.java
@@ -14,13 +14,13 @@ import org.nrg.framework.task.XnatTask;
 import org.nrg.framework.task.services.XnatTaskService;
 import org.nrg.xnat.services.XnatAppInfo;
 import org.nrg.xnat.task.AbstractXnatTask;
-import org.springframework.jdbc.core.JdbcTemplate1;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @XnatTask(taskId = "Template1Task", description = "Template1 Task", defaultExecutionResolver = "SingleNodeExecutionResolver", executionResolverConfigurable = true)
 @Slf4j
 public class Template1Task extends AbstractXnatTask {
-    public Template1Task(final XnatTaskService taskService, final XnatAppInfo appInfo, final JdbcTemplate1 jdbcTemplate1) {
-        super(taskService, true, appInfo, jdbcTemplate1);
+    public Template1Task(final XnatTaskService taskService, final XnatAppInfo appInfo, final JdbcTemplate jdbcTemplate) {
+        super(taskService, true, appInfo, jdbcTemplate);
     }
 
     @Override
